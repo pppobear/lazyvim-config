f7751587a22d24a8358ecd75e9676fdd4196e573
pppobear <pppobear@gmail.com>
2025-10-21 21:26:51 +0800

    update

--- lua/plugins/claude-code.lua
+++ lua/plugins/claude-code.lua
@@ -1,4 +1,4 @@
-local toggle_key = "<leader>c1"
+local toggle_key = "<A-,>"
 return {
   {
     "coder/claudecode.nvim",
@@ -8,8 +8,26 @@ return {
       { toggle_key, "<cmd>ClaudeCodeFocus<cr>", desc = "Claude Code", mode = { "n", "x" } },
     },
     opts = {
+      terminal_cmd = "/Users/suyihang/.claude/local/claude",
       terminal = {
-        provider = "native",
+        ---@module "snacks"
+        ---@type snacks.win.Config|{}
+        snacks_win_opts = {
+          position = "bottom",
+          width = 1.0,
+          height = 0.4,
+          border = "single",
+          keys = {
+            claude_hide = {
+              toggle_key,
+              function(self)
+                self:hide()
+              end,
+              mode = "t",
+              desc = "Hide",
+            },
+          },
+        },
       },
     },
   },
